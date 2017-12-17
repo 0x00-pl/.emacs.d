2f763f1231bcdb911e0a6e7e4e81943edb563406
0x00-pl <0x00.pl@gmail.com>
2017-12-17 10:14:14 +0800

    add more packages

--- init.el
+++ init.el
@@ -6,13 +6,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(backup-directory-alist (quote (("." . "~/.emacs.d/.saves"))))
+ '(company-backends
+   (quote
+    (company-bbdb company-nxml company-css company-eclim company-semantic company-clang company-xcode company-cmake company-capf company-files
+		  (company-dabbrev-code company-gtags company-etags company-keywords)
+		  company-oddmuse company-dabbrev company-tern)))
  '(inhibit-startup-screen t)
  '(package-archives
    (quote
     (("gnu" . "https://elpa.gnu.org/packages/")
      ("marmalade" . "https://marmalade-repo.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
- '(package-selected-packages (quote (ac-js2 js2-mode editorconfig vue-mode company))))
+ '(package-selected-packages
+   (quote
+    (company-web company-tern js-format guide-key web-mode ac-js2 js2-mode editorconfig vue-mode company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -28,5 +35,15 @@
 (package-install-selected-packages)
 
 
+(add-hook 'after-init-hook 'global-company-mode)
+
 (require 'editorconfig)
 (editorconfig-mode 1)
+
+(require 'guide-key)
+(guide-key-mode 1)
+
+
+(require 'js-format)
+
+
